24dec18ef935610236185ec5b400d7bdb3129009
Dhrumisha Rakholiya <dhrumisharakholiya12@gmail.com>
2025-02-17 10:37:08 +0530

    chart changes

---
+++ frontend/src/data/chart.ts
@@ -0,0 +1,16 @@
+export const data01 = [
+  { name: "Dashboard", users: 38, color: "#8884d8", visible: true },
+  { name: "Master Product Feed", users: 38, color: "#82ca9d", visible: true },
+  { name: "Stores", users: 38, color: "#ffc658", visible: true },
+  { name: "Content Management", users: 38, color: "#ff7300", visible: true },
+  { name: "Orders", users: 38, color: "#d0ed57", visible: true },
+  { name: "Customer", users: 38, color: "#a4de6c", visible: true },
+  { name: "Promotions", users: 38, color: "#8884d8", visible: true },
+  { name: "Settings", users: 38, color: "#82ca9d", visible: true },
+  { name: "Configurator", users: 38, color: "#ffc658", visible: true },
+  { name: "Google Analytics", users: 31, color: "#ff7300", visible: true },
+  { name: "Dashboard Widget", users: 31, color: "#d0ed57", visible: true },
+  { name: "Reports", users: 38, color: "#a4de6c", visible: true },
+  { name: "Weight Master", users: 31, color: "#8884d8", visible: true }
+  ];
+ 
